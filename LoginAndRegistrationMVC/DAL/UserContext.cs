e3036c90f799d2b4f32f16c8fe15cda5e538cf8b
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2021-03-02 12:56:39 +0000

    feature: added account creation and login, exercise is terminated

---
+++ LoginAndRegistrationMVC/DAL/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using LoginAndRegistrationMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace LoginAndRegistrationMVC.DAL
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("AuthDatabase") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
